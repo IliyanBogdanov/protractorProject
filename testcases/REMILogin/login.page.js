02b053a79edcbdfcaef014284e9dc39969c9e441
iliyan.bogdanov <iliyanbogdanov@gmail.com>
2017-10-24 13:48:14 +0300

    loginPage waits and messages

--- testcases/REMILogin/login.page.js
+++ testcases/REMILogin/login.page.js
@@ -16,29 +16,29 @@ var loginPage = function(){
     };
 
     this.clickLogin = function() {
-        browser.wait(EC.elementToBeClickable($('button[type="button"]')), 5000);
+        browser.wait(EC.elementToBeClickable($('button[type="button"]')), 10000, 'Wait for clickLogin has failed.');
         element(by.css('button[type="button"]')).click();
     };
 
     this.navigateToRemisDev = function() {
         browser.get('http://remisdev.demos.bgosoftware.com/login/');
-        browser.wait(EC.elementToBeClickable($('button[type="button"]')), 5000);
+        browser.wait(EC.elementToBeClickable($('button[type="button"]')), 10000, 'Wait for navigateToRemisDev has failed.');
     };
 
     this.loginREMI = function(username, passowrd) {
         var setUsername = (username) ? username : 'martouser1';
         var setPassword = (passowrd) ? passowrd : '25Kukuvici';
         browser.executeScript("var test = $('#remis-login-domain').data('kendoDropDownList');test.select(4);test.trigger('change');");
-        browser.wait(EC.elementToBeClickable($('#remis-login-username')), 5000);
+        browser.wait(EC.elementToBeClickable($('#remis-login-username')), 10000, 'Wait for #remis-login-username has failed.');
         element(by.id('remis-login-username')).click().sendKeys(setUsername);
-        browser.wait(EC.elementToBeClickable($('#remis-login-password')), 5000);
+        browser.wait(EC.elementToBeClickable($('#remis-login-password')), 10000, 'Wait for #remis-login-password has failed.');
         element(by.id('remis-login-password')).click().sendKeys(setPassword);
-        browser.wait(EC.elementToBeClickable($('button[type="button"]')), 5000);
+        browser.wait(EC.elementToBeClickable($('button[type="button"]')), 10000, 'Wait for button[type="button"] has failed.');
         element(by.css('button[type="button"]')).click();
     };
 
     this.logoutREMI = function() {
-        browser.wait(EC.elementToBeClickable($('#wrapper > div > div > header > nav > div > ul > li:nth-child(2) > a > i')), 5000);
+        browser.wait(EC.elementToBeClickable($('#wrapper > div > div > header > nav > div > ul > li:nth-child(2) > a > i')), 10000, 'Wait for logoutREMI has failed.');
         element(by.css('#wrapper > div > div > header > nav > div > ul > li:nth-child(2) > a > i')).click();
         element(by.buttonText('Yes')).click();  
     };
